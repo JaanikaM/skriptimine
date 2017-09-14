0de0463510adfbdf35550bdc282aed6291c5a0ee
root <root@jaanika>
2017-09-14 09:51:31 +0300

    lisa kasutaja

---
+++ LINUX/lisa_kasutaja.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#Jaanika Muurisepp
+# 14.09.2017
+#Kasutaja lisamine ilma paroolita
+#
+clear
+#
+kasutajanimi=$1
+#loome kasutaja koos kodukataloogiga. v6ti -m
+#muidu l6petame skript veateate signaaliga 1
+useradd $kasutajanimi || exit 1
+# lisame kasutajale bash kooriku, kuna vaikimisi on see sh koorikuga
+usermod -s /bin/bash $kasutajanimi || exit 1
+#vajadusel, kui meil olemas spetskasutaja kodukataloogi s2tted
+cp /etc/skel/.* /home/$kasutajanimi || exit
